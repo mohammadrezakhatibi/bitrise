918ba00c7293aad6754631c31cafd817ace2c3c2
MohammadReza Khatibi <mohammadrezakhatibi@outlook.com>
2020-12-22 15:43:09 +0330

    git init

---
+++ bitrise/bitrise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitrise
+//
+//  Created by MohammadReza Khatibi on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
